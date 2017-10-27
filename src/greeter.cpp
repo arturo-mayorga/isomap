7698b0c3f44dae0cb927e70dc5306efa16429787
Arturo Mayorga <arturo.mayorga@protonmail.com>
2017-10-27 08:39:00 -0400

    adding makefile

---
+++ src/greeter.cpp
@@ -0,0 +1,8 @@
+#include "greeter.h"
+#include <iostream>
+using namespace std;
+
+void Greeter::greet()
+{
+    cout << "Greeter class here!" << endl;
+}
---
+++ test/cli.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "greeter.h"
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Welcome to C++ Programming!!" << endl;
+
+    Greeter g;
+
+
+    g.greet();
+
+
+
+    return 0;
+}
+
+
+
