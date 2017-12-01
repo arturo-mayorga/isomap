621f6204a765a957a93fa96c66da9ce036432467
arturo-mayorga <arturo.mayorga@gmail.com>
2017-12-01 02:10:31 -0500

    Exposing manifold topology.

--- src/manifolddistanceprocessor.h
+++ src/manifolddistanceprocessor.h
@@ -2,11 +2,13 @@
 #define MANIFOLDDISTANCEPROCESSOR_H
 
 #include <distanceprocessor.h>
+#include <manifoldnode.h>
 
 class ManifoldDistanceProcessor: public DistanceProcessor
 {
     public:
     virtual std::shared_ptr<DissimilarityMatrix> getDissimilarityMatrix();
+    std::vector<ManifoldNode*> getManifoldTopology();
 };
 
 #endif
